53f1ce1e59b7dd6145a3d8e04bd70fa569836802
Francois Lanusse <fr.eiffel@gmail.com>
2023-07-04 21:11:38 +0200

    added more functions to dataset conversion

--- scripts/convertDataSet.py
+++ scripts/convertDataSet.py
@@ -1,57 +1,88 @@
-
-''''''
+""" Convert the GZ talk comments CSV file to a JSON file for the ZooConvo dataset.
+"""
 import pandas as pd
 import json
 from tqdm import tqdm
+import urllib.request
+import os
+from absl import app, flags
+from absl.flags import FLAGS
 
-# Read the CSV file into a DataFrame
-data = pd.read_csv('GZ_talk_comments_notes_urls_AISSAI.csv')
+# Define the command line arguments
+flags.DEFINE_string('input_csv', 'GZ_talk_comments_notes_urls_AISSAI.csv', 'Path to the input CSV file')
+flags.DEFINE_string('output_folder', 'galaxyzoo_dataset', 'Path to the output folder')
+# Add a flag to download images
+flags.DEFINE_bool('download_images', False, 'Download images from the URLs in the CSV file')
 
-# Group the data by "subject_id"
-grouped_data = data.groupby('subject_id')
+FLAGS = flags.FLAGS
 
-# Create a list to store the grouped data as dictionaries
-grouped_data_list = []
+# Define the main function
+def main(_argv):
 
-# Initialize the progress bar
-progress_bar = tqdm(total=len(grouped_data), desc="Processing")
+    # Create the output folder if it does not exist
+    if not os.path.exists(FLAGS.output_folder):
+        os.makedirs(FLAGS.output_folder)
 
-# Iterate over the groups and populate the grouped data list
-for discussion_id, group in grouped_data:
-    sentences = group['comment_body'].tolist()
+    # Read the CSV file into a DataFrame
+    data = pd.read_csv(FLAGS.input_csv)
 
-    # Extract the first entry from the "locations" column
-    location_entry = group['locations'].iloc[0]
-    #need that because of https://
-    image = ((location_entry.split(":")[1])+":"+(location_entry.split(":")[2])).strip('\"').split("jpeg")[0]+"jpeg"
+    # Group the data by "subject_id"
+    grouped_data = data.groupby('subject_id')
 
-    # Create a dictionary for each group
-    group_dict = {
-        "id": str(discussion_id).split(".")[0],
-        "image": image,
-        "conversations": []
-    }
+    # Create a list to store the grouped data as dictionaries
+    grouped_data_list = []
 
-    # Add sentences to the conversations list
-    for sentence in sentences:
-        conversation = {
-            "from": "human",
-            "value": sentence
+    # Initialize the progress bar
+    progress_bar = tqdm(total=len(grouped_data), desc="Processing")
+
+    # Iterate over the groups and populate the grouped data list
+    for discussion_id, group in grouped_data:
+        sentences = group['comment_body'].tolist()
+
+        # Extract the first entry from the "locations" column
+        location_entry = group['locations'].iloc[0]
+        #need that because of https://
+        image = ((location_entry.split(":")[1])+":"+(location_entry.split(":")[2])).strip('\"').split("jpeg")[0]+"jpeg"
+
+        # Create a dictionary for each group
+        group_dict = {
+            "id": str(discussion_id).split(".")[0],
+            "image": image,
+            "conversations": []
         }
-        group_dict["conversations"].append(conversation)
 
-    # Append the group dictionary to the list
-    grouped_data_list.append(group_dict)
+        # Add sentences to the conversations list
+        for sentence in sentences:
+            conversation = {
+                "from": "human",
+                "value": sentence
+            }
+            group_dict["conversations"].append(conversation)
 
-    # Update the progress bar
-    progress_bar.update(1)
+        # Append the group dictionary to the list
+        grouped_data_list.append(group_dict)
 
-# Close the progress bar
-progress_bar.close()
+        # Download the image if the flag is set
+        if FLAGS.download_images:
+            # Create the output path
+            output_path = FLAGS.output_folder + "/" + str(discussion_id).split(".")[0] + ".jpeg"
 
-# Convert the grouped data list to JSON
-json_data = json.dumps(grouped_data_list, indent=4)
+            # Download the image
+            urllib.request.urlretrieve(image, output_path)
 
-# Write the JSON data to a file
-with open('output.json', 'w') as file:
-    file.write(json_data)
+        # Update the progress bar
+        progress_bar.update(1)
+
+    # Close the progress bar
+    progress_bar.close()
+
+    # Convert the grouped data list to JSON
+    json_data = json.dumps(grouped_data_list, indent=4)
+
+    # Write the JSON metadata to a file
+    with open( FLAGS.output_folder + "/metadata.json", 'w') as file:
+        file.write(json_data)
+
+# Call the main function
+if __name__ == '__main__':
+    app.run(main)
