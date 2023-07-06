c8707d2ac0d50d30d8e440480297cd239c0a52d9
guler <guler@>
2023-07-06 13:55:29 +0200

    Updates in llavastro with image Ids in outfile and Slurm file to launch

--- scripts/apply_llavastro.py
+++ scripts/apply_llavastro.py
@@ -1,26 +1,31 @@
 import argparse
-from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig
-import torch
-import os
-import json
-from tqdm import tqdm
-import shortuuid
 import glob
+import json
+import math
+import os
+import random
 
+import shortuuid
+import torch
 from llava import LlavaLlamaForCausalLM
 from llava.conversation import conv_templates
 from llava.utils import disable_torch_init
-from transformers import CLIPVisionModel, CLIPImageProcessor, StoppingCriteria
-
 from PIL import Image
-import random
-import math
+from tqdm import tqdm
+from transformers import (
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    CLIPImageProcessor,
+    CLIPVisionModel,
+    StoppingCriteria,
+)
 
 
 def split_list(lst, n):
     """Split a list into n (roughly) equal-sized chunks"""
     chunk_size = math.ceil(len(lst) / n)  # integer division
-    return [lst[i:i+chunk_size] for i in range(0, len(lst), chunk_size)]
+    return [lst[i : i + chunk_size] for i in range(0, len(lst), chunk_size)]
 
 
 def get_chunk(lst, n, k):
@@ -32,8 +37,6 @@ DEFAULT_IMAGE_TOKEN = "<image>"
 DEFAULT_IMAGE_PATCH_TOKEN = "<im_patch>"
 DEFAULT_IM_START_TOKEN = "<im_start>"
 DEFAULT_IM_END_TOKEN = "<im_end>"
-
-
 
 
 detail_describe_instructions = [
@@ -78,12 +81,14 @@ def patch_config(config):
     patch_dict = {
         "use_mm_proj": True,
         "mm_vision_tower": "openai/clip-vit-large-patch14",
-        "mm_hidden_size": 1024
+        "mm_hidden_size": 1024,
     }
 
     cfg = AutoConfig.from_pretrained(config)
     if not hasattr(cfg, "mm_vision_tower"):
-        print(f'`mm_vision_tower` not found in `{config}`, applying patch and save to disk.')
+        print(
+            f"`mm_vision_tower` not found in `{config}`, applying patch and save to disk."
+        )
         for k, v in patch_dict.items():
             setattr(cfg, k, v)
         cfg.save_pretrained(config)
@@ -97,11 +102,15 @@ class KeywordsStoppingCriteria(StoppingCriteria):
         self.start_len = None
         self.input_ids = input_ids
 
-    def __call__(self, output_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+    def __call__(
+        self, output_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs
+    ) -> bool:
         if self.start_len is None:
             self.start_len = self.input_ids.shape[1]
         else:
-            outputs = self.tokenizer.batch_decode(output_ids[:, self.start_len:], skip_special_tokens=True)[0]
+            outputs = self.tokenizer.batch_decode(
+                output_ids[:, self.start_len :], skip_special_tokens=True
+            )[0]
             for keyword in self.keywords:
                 if keyword in outputs:
                     return True
@@ -115,56 +124,90 @@ def eval_model(args):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     if args.mm_projector is None:
         patch_config(model_name)
-        model = LlavaLlamaForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, use_cache=True).cuda()
-        image_processor = CLIPImageProcessor.from_pretrained(model.config.mm_vision_tower, torch_dtype=torch.float16)
+        model = LlavaLlamaForCausalLM.from_pretrained(
+            model_name, torch_dtype=torch.float16, use_cache=True
+        ).cuda()
+        image_processor = CLIPImageProcessor.from_pretrained(
+            model.config.mm_vision_tower, torch_dtype=torch.float16
+        )
 
         mm_use_im_start_end = getattr(model.config, "mm_use_im_start_end", False)
         tokenizer.add_tokens([DEFAULT_IMAGE_PATCH_TOKEN], special_tokens=True)
         if mm_use_im_start_end:
-            tokenizer.add_tokens([DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN], special_tokens=True)
+            tokenizer.add_tokens(
+                [DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN], special_tokens=True
+            )
 
         vision_tower = model.model.vision_tower[0]
-        vision_tower.to(device='cuda', dtype=torch.float16)
+        vision_tower.to(device="cuda", dtype=torch.float16)
         vision_config = vision_tower.config
-        vision_config.im_patch_token = tokenizer.convert_tokens_to_ids([DEFAULT_IMAGE_PATCH_TOKEN])[0]
+        vision_config.im_patch_token = tokenizer.convert_tokens_to_ids(
+            [DEFAULT_IMAGE_PATCH_TOKEN]
+        )[0]
         vision_config.use_im_start_end = mm_use_im_start_end
         if mm_use_im_start_end:
-            vision_config.im_start_token, vision_config.im_end_token = tokenizer.convert_tokens_to_ids([DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN])
+            (
+                vision_config.im_start_token,
+                vision_config.im_end_token,
+            ) = tokenizer.convert_tokens_to_ids(
+                [DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN]
+            )
         image_token_len = (vision_config.image_size // vision_config.patch_size) ** 2
     else:
         # in case of using a pretrained model with only a MLP projector weights
-        model = LlavaLlamaForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, use_cache=True).cuda()
+        model = LlavaLlamaForCausalLM.from_pretrained(
+            model_name, torch_dtype=torch.float16, use_cache=True
+        ).cuda()
 
         mm_use_im_start_end = getattr(model.config, "mm_use_im_start_end", False)
         tokenizer.add_tokens([DEFAULT_IMAGE_PATCH_TOKEN], special_tokens=True)
         if mm_use_im_start_end:
-            tokenizer.add_tokens([DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN], special_tokens=True)
+            tokenizer.add_tokens(
+                [DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN], special_tokens=True
+            )
 
-        vision_tower = CLIPVisionModel.from_pretrained(args.vision_tower, torch_dtype=torch.float16).cuda()
-        image_processor = CLIPImageProcessor.from_pretrained(args.vision_tower, torch_dtype=torch.float16)
+        vision_tower = CLIPVisionModel.from_pretrained(
+            args.vision_tower, torch_dtype=torch.float16
+        ).cuda()
+        image_processor = CLIPImageProcessor.from_pretrained(
+            args.vision_tower, torch_dtype=torch.float16
+        )
 
         vision_config = vision_tower.config
-        vision_config.im_patch_token = tokenizer.convert_tokens_to_ids([DEFAULT_IMAGE_PATCH_TOKEN])[0]
+        vision_config.im_patch_token = tokenizer.convert_tokens_to_ids(
+            [DEFAULT_IMAGE_PATCH_TOKEN]
+        )[0]
         vision_config.use_im_start_end = mm_use_im_start_end
         if mm_use_im_start_end:
-            vision_config.im_start_token, vision_config.im_end_token = tokenizer.convert_tokens_to_ids([DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN])
+            (
+                vision_config.im_start_token,
+                vision_config.im_end_token,
+            ) = tokenizer.convert_tokens_to_ids(
+                [DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN]
+            )
 
         image_token_len = (vision_config.image_size // vision_config.patch_size) ** 2
 
-        mm_projector = torch.nn.Linear(vision_config.hidden_size, model.config.hidden_size)
-        mm_projector_weights = torch.load(args.mm_projector, map_location='cpu')
-        mm_projector.load_state_dict({k.split('.')[-1]: v for k, v in mm_projector_weights.items()})
+        mm_projector = torch.nn.Linear(
+            vision_config.hidden_size, model.config.hidden_size
+        )
+        mm_projector_weights = torch.load(args.mm_projector, map_location="cpu")
+        mm_projector.load_state_dict(
+            {k.split(".")[-1]: v for k, v in mm_projector_weights.items()}
+        )
 
         model.model.mm_projector = mm_projector.cuda().half()
         model.model.vision_tower = [vision_tower]
 
-    #questions = json.load(open(os.path.expanduser(args.question_file), "r"))
-    #questions = get_chunk(questions, args.num_chunks, args.chunk_idx)
+    # questions = json.load(open(os.path.expanduser(args.question_file), "r"))
+    # questions = get_chunk(questions, args.num_chunks, args.chunk_idx)
     answers_file = os.path.expanduser(args.answers_file)
     os.makedirs(os.path.dirname(answers_file), exist_ok=True)
     os.makedirs(os.path.join(os.path.dirname(answers_file), "images"), exist_ok=True)
     ans_file = open(answers_file, "w")
-    save_image_folder = os.path.join(os.path.dirname(os.path.expanduser(args.answers_file)), "images")
+    save_image_folder = os.path.join(
+        os.path.dirname(os.path.expanduser(args.answers_file)), "images"
+    )
 
     # Build a set of image paths using the image folder with glob
     image_paths = glob.glob(os.path.join(args.image_folder, "*"))
@@ -175,33 +218,42 @@ def eval_model(args):
 
         # question = line['conversations'][0]
         # gt_ans = line["conversations"][1]
-        
+
         # qs = question['value']
 
-        qs = qs.replace('<image>', '').strip()
+        qs = qs.replace("<image>", "").strip()
         cur_prompt = qs
 
-        
         image_file = image_paths[idx % len(image_paths)]
-        image_id = os.path.splitext(image_file)[0].split('/')[-1] # extract the last name without the extension
+        image_id = os.path.splitext(image_file)[0].split("/")[
+            -1
+        ]  # extract the last name without the extension
         # print(f' qs {qs}  cur_prompt {cur_prompt}')
-        # print(f'------ idx : {idx} imahe file : {image_file} len im paths : {len(image_paths)} image_id {image_id}')        
+        # print(f'------ idx : {idx} imahe file : {image_file} len im paths : {len(image_paths)} image_id {image_id}')
 
         # if 'image' in line:
         #     image_file = line["image"]
         image = Image.open(os.path.join(args.image_folder, image_file))
-        image_tensor = image_processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
+        image_tensor = image_processor.preprocess(image, return_tensors="pt")[
+            "pixel_values"
+        ][0]
         images = image_tensor.unsqueeze(0).half().cuda()
-        if getattr(model.config, 'mm_use_im_start_end', False):
-            qs = qs + '\n' + DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_PATCH_TOKEN * image_token_len + DEFAULT_IM_END_TOKEN
+        if getattr(model.config, "mm_use_im_start_end", False):
+            qs = (
+                qs
+                + "\n"
+                + DEFAULT_IM_START_TOKEN
+                + DEFAULT_IMAGE_PATCH_TOKEN * image_token_len
+                + DEFAULT_IM_END_TOKEN
+            )
         else:
-            qs = qs + '\n' + DEFAULT_IMAGE_PATCH_TOKEN * image_token_len
-        cur_prompt = cur_prompt + '\n' + '<image>'
+            qs = qs + "\n" + DEFAULT_IMAGE_PATCH_TOKEN * image_token_len
+        cur_prompt = cur_prompt + "\n" + "<image>"
         # else:
         #     images = None
 
-        if args.conv_mode == 'simple_legacy':
-            qs += '\n\n### Response:'
+        if args.conv_mode == "simple_legacy":
+            qs += "\n\n### Response:"
         # assert gt_ans['from'] == 'gpt'
         # conv = default_conversation.copy()
         conv = conv_templates[args.conv_mode].copy()
@@ -211,7 +263,7 @@ def eval_model(args):
 
         input_ids = torch.as_tensor(inputs.input_ids).cuda()
 
-        keywords = ['###']
+        keywords = ["###"]
         stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
 
         with torch.inference_mode():
@@ -221,24 +273,31 @@ def eval_model(args):
                 do_sample=True,
                 temperature=0.7,
                 max_new_tokens=1024,
-                stopping_criteria=[stopping_criteria])
-#            print('output_ids --> ', output_ids)
-#            print('input_ids --> ', input_ids)
+                stopping_criteria=[stopping_criteria],
+            )
+        #            print('output_ids --> ', output_ids)
+        #            print('input_ids --> ', input_ids)
 
         # TODO: new implementation
         input_token_len = input_ids.shape[1]
-        n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
+        n_diff_input_output = (
+            (input_ids != output_ids[:, :input_token_len]).sum().item()
+        )
         if n_diff_input_output > 0:
-            print(f'[Warning] Sample {i}: {n_diff_input_output} output_ids are not the same as the input_ids')
-        outputs = tokenizer.batch_decode(output_ids[:, input_token_len:], skip_special_tokens=True)[0]
+            print(
+                f"[Warning] Sample {i}: {n_diff_input_output} output_ids are not the same as the input_ids"
+            )
+        outputs = tokenizer.batch_decode(
+            output_ids[:, input_token_len:], skip_special_tokens=True
+        )[0]
 
-        if args.conv_mode == 'simple_legacy':
+        if args.conv_mode == "simple_legacy":
             while True:
                 cur_len = len(outputs)
                 outputs = outputs.strip()
-                for pattern in ['###', 'Assistant:', 'Response:']:
+                for pattern in ["###", "Assistant:", "Response:"]:
                     if outputs.startswith(pattern):
-                        outputs = outputs[len(pattern):].strip()
+                        outputs = outputs[len(pattern) :].strip()
                 if len(outputs) == cur_len:
                     break
 
@@ -253,11 +312,11 @@ def eval_model(args):
         # prompt for answer
         if args.answer_prompter:
             outputs_reasoning = outputs
-            inputs = tokenizer([prompt + outputs_reasoning + ' ###\nANSWER:'])
+            inputs = tokenizer([prompt + outputs_reasoning + " ###\nANSWER:"])
 
             input_ids = torch.as_tensor(inputs.input_ids).cuda()
 
-            keywords = ['###']
+            keywords = ["###"]
             stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
 
             with torch.inference_mode():
@@ -267,13 +326,20 @@ def eval_model(args):
                     do_sample=True,
                     temperature=0.7,
                     max_new_tokens=64,
-                    stopping_criteria=[stopping_criteria])
+                    stopping_criteria=[stopping_criteria],
+                )
 
             input_token_len = input_ids.shape[1]
-            n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
+            n_diff_input_output = (
+                (input_ids != output_ids[:, :input_token_len]).sum().item()
+            )
             if n_diff_input_output > 0:
-                print(f'[Warning] Sample {i}: {n_diff_input_output} output_ids are not the same as the input_ids')
-            outputs = tokenizer.batch_decode(output_ids[:, input_token_len:], skip_special_tokens=True)[0]
+                print(
+                    f"[Warning] Sample {i}: {n_diff_input_output} output_ids are not the same as the input_ids"
+                )
+            outputs = tokenizer.batch_decode(
+                output_ids[:, input_token_len:], skip_special_tokens=True
+            )[0]
 
             try:
                 index = outputs.index(conv.sep)
@@ -282,7 +348,7 @@ def eval_model(args):
                 index = outputs.index(conv.sep)
 
             outputs = outputs[:index].strip()
-            outputs = outputs_reasoning + '\n The answer is ' + outputs
+            outputs = outputs_reasoning + "\n The answer is " + outputs
 
         # new implementation ends
 
@@ -296,17 +362,24 @@ def eval_model(args):
 
         # outputs = outputs[len(prompt) + len(conv.roles[1]) + 2:index].strip()
 
-
         ans_id = shortuuid.uuid()
-        ans_file.write(json.dumps({"image_id":image_id,
-                                   "question_id": idx,
-                                   "prompt": cur_prompt,
-                                   "text": outputs,
-                                   "answer_id": ans_id,
-                                   "model_id": model_name,
-                                   "metadata": {}}) + "\n")
+        ans_file.write(
+            json.dumps(
+                {
+                    "image_id": image_id,
+                    "question_id": idx,
+                    "prompt": cur_prompt,
+                    "text": outputs,
+                    "answer_id": ans_id,
+                    "model_id": model_name,
+                    "metadata": {},
+                }
+            )
+            + "\n"
+        )
         ans_file.flush()
     ans_file.close()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
