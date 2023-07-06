8e581419daf55bb68089a6958c77416f5365ed0d
Francois Lanusse <fr.eiffel@gmail.com>
2023-07-06 17:11:56 +0200

    minor modifications of the script

--- scripts/train_v0.sh
+++ scripts/train_v0.sh
@@ -12,6 +12,7 @@
 #SBATCH --account=owt@a100
 #SBATCH -C a100
 
+cd /gpfswork/rech/owt/commun/lavastro_training
 module load cuda/11.7.1 nccl/2.12.12-1-cuda cudnn/8.5.0.96-11.7-cuda gcc/8.5.0 openmpi/4.1.1-cuda intel-mkl/2020.4 magma/2.7.0-cuda sox/14.4.2 sparsehash/2.0.3 libjpeg-turbo/2.1.3 python/3.9.12
 eval $(idrenv -d owt)
 
