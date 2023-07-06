1fd24b799cfbb1b36660990b0e3b96bb88b6cc3d
Michael Dell'aiera <michael.dellaiera@lapp.in2p3.fr>
2023-07-06 19:49:07 +0200

    minor fix on slurm job

--- scripts/slurm_job_generate_dataset_v0.sh
+++ scripts/slurm_job_generate_dataset_v0.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=generate_qa
-#SBATCH --output=%x%j.out    # job-name + jobid
-#SBATCH --error=%x%j.out    #
+#SBATCH --job-name=generate_qa_v0
+##SBATCH --output=%x%j.out    # job-name + jobid
+#SBATCH --error=%x%j.err    #
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=10   # 8 per GPU
+#SBATCH --partition=compil
+#SBATCH --cpus-per-task=2   
 #SBATCH --hint=nomultithread 
 #SBATCH --time=20:00:00
 #SBATCH --account=owt@v100
 
-module load cuda/11.7.1 nccl/2.12.12-1-cuda cudnn/8.5.0.96-11.7-cuda gcc/8.5.0 openmpi/4.1.1-cuda intel-mkl/2020.4 magma/2.7.0-cuda sox/14.4.2 sparsehash/2.0.3 libjpeg-turbo/2.1.3 python/3.9.12
-
-export PYTHONUSERBASE=$ALL_CCFRWORK/galaxy_zoo/.local_sample
-export PATH=/gpfswork/rech/owt/commun/galaxy_zoo/.local_sample/bin/:$PATH
+module python/3.9.12
+export PYTHONUSERBASE=/gpfswork/rech/owt/uka17ma/galaxy_zoo/.local
+export PATH=$PATH:$PYTHONUSERBASE/bin
+source $HOME/.bashrc
 
 ## for debugging
 set -x
